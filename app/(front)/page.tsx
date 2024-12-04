f5bd527a881b74c861ac570ca4b95ffed6bf3f30
Theo-Smondack <smondack.theo.pro@gmail.com>
2024-12-04 17:10:12 +0100

    Add loading property to button

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -7,17 +7,26 @@ import { Button } from '@/components/ui/button';
 
 export default function Home() {
     const [news, setNews] = useState<string | string[] | undefined>(undefined);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const clearNews = () => {
+        setNews(undefined);
+    }
+
     const onClickGenerate = async () => {
+        clearNews();
+        setLoading(true);
         const res = await getPageTitle();
-        if (res.result) {
-            setNews(res.result);
+        setLoading(false);
+        if (res.title) {
+            setNews(res.title);
             return;
         }
     }
     return (
         <div className="flex w-full flex-col items-center justify-center md:gap-[32px] gap-[24px] px-6">
             <h1 className="md:text-4xl font-bold text-center text-2xl">Click on generate to get news about cryptocurrency market !</h1>
-            <Button size="lg" className="md:text-2xl text-xl" onClick={onClickGenerate}>Generate</Button>
+            <Button size="lg" className="md:text-2xl text-xl" onClick={onClickGenerate} loading={loading}>Generate</Button>
             <NewsCard news={news} />
         </div>
     );
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,5 +1,6 @@
-import { Slot } from '@radix-ui/react-slot';
+import { Slot, Slottable } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
+import { Loader2 } from 'lucide-react';
 import * as React from 'react';
 
 import { cn } from '@/lib/utils';
@@ -36,17 +37,22 @@ export interface ButtonProps
     extends React.ButtonHTMLAttributes<HTMLButtonElement>,
         VariantProps<typeof buttonVariants> {
     asChild?: boolean;
+    loading?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-    ({ className, variant, size, asChild = false, ...props }, ref) => {
+    ({ className, variant, size, loading = false, asChild = false, children, disabled, ...props }, ref) => {
         const Comp = asChild ? Slot : 'button';
         return (
             <Comp
                 className={cn(buttonVariants({ variant, size, className }))}
                 ref={ref}
+                disabled={loading || disabled}
                 {...props}
-            />
+            >
+                {loading && <Loader2 className="mr-2 h-5 w-5 animate-spin"/>}
+                <Slottable>{children}</Slottable>
+            </Comp>
         );
     },
 );
